4ca96e0b6025a1a9654337e7a47aed4645ae6bb4
Xiangwen Liu <xxliu10@linux.local>
2018-01-11 21:41:52 -0600

    .

--- record.py
+++ record.py
@@ -1,4 +1,12 @@
 running records on KATE:
+
+python construct_20news.py -train train/allnews/ -test test/allnews/ -o outdir
+python train.py -i outdir/train.corpus  -nv 9000 --noise gs  -ctype kcomp -nd 2000 -sm model_2000
+python pred.py -i bbcdata_prepare/train.corpus  -lm model_2000  -o output/training_2000.txt -e output/tr_embedding.txt
+python pred.py -i bbcdata_prepare/test.corpus  -lm model_2000  -o output/test_2000.txt -e output/te_embedding.txt
+python run_classifier.py output/training_2000.txt bbcdata_prepare/train.labels output/test_2000.txt bbcdata_prepare/test.labels -nv 200 -cv 10
+----------------------------------------
+
 python construct_20news.py -train train/allnews/ -test test/allnews/ -o outdir
 python train.py -i outdir/train.corpus  -nv 9000 --noise gs  -ctype kcomp -nd 2000
 python pred.py -i bbcdata_prepare/train.corpus  -lm model  -o output/training.txt -e output/tr_embedding.txt
