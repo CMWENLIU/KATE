a5b4b41fe1691820c7695fdc1a2504b193f75cd7
Xiangwen Liu <xxliu10@linux.local>
2018-01-16 14:09:00 -0600

    .

--- record.py
+++ record.py
@@ -2,6 +2,11 @@ running records on KATE:
 
 python construct_20news.py -train mixmodel/mixdata/ -test test/allnews/ -o mixmodel/classf_test/
 python construct_20news.py -train mixmodel/mixdata/ -test train/allnews/ -o mixmodel/classf_train/
+python train.py -i mixmodel/classf_train/train.corpus  -nv 10000 --noise gs  -ctype kcomp -nd 2000 -sm mixmodel/model_2000
+python pred.py -i mixmodel/classf_train/test.corpus  -lm mixmodel/model_2000  -o mixmodel/training_2000.txt -e mixmodel/tr_embedding.txt
+python pred.py -i mixmodel/classf_test/test.corpus  -lm mixmodel/model_2000  -o mixmodel/test_2000.txt -e mixmodel/te_embedding.txt
+python run_classifier.py mixmodel/training_2000.txt mixmodel/classf_train/test.labels mixmodel/test_2000.txt mixmodel/classf_test/test.labels -nv 2000 -cv 10
+python run_classifier.py mixmodel/tr_embedding.txt  mixmodel/classf_train/test.labels mixmodel/te_embedding.txt  mixmodel/classf_test/test.labels -nv 2000 -cv 10
 -------------------------------------------
 python construct_20news.py -train train/allnews/ -test test/allnews/ -o outdir
 python train.py -i outdir/train.corpus  -nv 9000 --noise gs  -ctype kcomp -nd 2000 -sm model_2000
